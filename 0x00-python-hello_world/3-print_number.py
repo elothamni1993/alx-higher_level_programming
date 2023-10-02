068d5895a5513113e1b608f9e275188965f99ee9
OTMANE El OTHMANI <elothmaniotmane00@gmail.com>
2023-10-02 05:16:19 -0700

    python

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 number = 98
-print("{:d} Battery street".format(number))
+
+# Use an f-string to format the integer and concatenate with " Battery street,"
+print(f"{number} Battery street")
