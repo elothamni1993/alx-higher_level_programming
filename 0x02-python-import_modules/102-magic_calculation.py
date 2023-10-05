7db1a0ab32d94c0ced6a37e1e7955d2c40bf9479
OTMANE El OTHMANI <elothmaniotmane00@gmail.com>
2023-10-05 10:23:11 -0700

    mod

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
-        add = a + b
-        for i in range(4, 6):
-            add = add + i
-        return add
+        c = add(a, b)
+        for n in range(4, 6):
+            c = add(c, n)
+        return c
     else:
-        return a - b
+        return sub(a, b)
