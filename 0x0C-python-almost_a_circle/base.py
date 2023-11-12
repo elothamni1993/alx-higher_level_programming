ad3aa8d773068ea929df1067fbcee83c3eaed439
OTMANE El OTHMANI <elothmaniotmane00@gmail.com>
2023-11-12 06:36:50 -0800

    a

---
+++ 0x0C-python-almost_a_circle/base.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''Module for Base class.'''
+from json import dumps, loads
+import csv
+
+
+class Base:
+    '''A representation of the base of our OOP hierarchy.'''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''Constructor.'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Jsonifies a dictionary so it's quite rightly and longer.'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''Unjsonifies a dictionary.'''
+        if json_string is None or not json_string:
+            return []
+        return loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''Saves jsonified object to file.'''
+        if list_objs is not None:
+            list_objs = [o.to_dictionary() for o in list_objs]
+        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_objs))
