6b96191bba372edf55858f64481d0b509845e06c
OTMANE El OTHMANI <elothmaniotmane00@gmail.com>
2023-11-14 07:59:34 -0800

    a

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,10 +2,8 @@
 '''Module for Square class.'''
 from models.rectangle import Rectangle
 
-
 class Square(Rectangle):
     '''A Square class.'''
-
     def __init__(self, size, x=0, y=0, id=None):
         '''Constructor.'''
         super().__init__(size, size, x, y, id)
